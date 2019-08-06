6a03b1b278804069f0f81615fd91be3e3a2cb2e8
Aveesh51 <aveeshshetty120@gmail.com>
2019-08-06 19:27:52 +0530

    Infinity scroll

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+  APPLICATION_ID: process.env.APPLICATION_ID || 'c4d0449f2c2e447ab9500adc441aec3539aafebcedae4bbb92538baf363809f8',
+  SECRET: process.env.SECRET || '7e4bfaed20dde9cb981c0e5d30bc4125c33177a4209f632eef5dc15c3cd8da8a',
+  CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+};
